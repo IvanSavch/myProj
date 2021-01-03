f84cd17176680a65df61ffa20eb537968e6d93b0
user <sav.van.ivan3@mail.ru>
2021-01-03 16:09:33 +0300

    MyProject

--- src/main/java/projectNomber1/repository/FlightRepository.java
+++ src/main/java/projectNomber1/repository/FlightRepository.java
@@ -1,7 +1,5 @@
 package projectNomber1.repository;
 
-import projectNomber1.model.Flight;
-
 import java.sql.SQLException;
 
 public interface FlightRepository {
--- src/main/java/projectNomber1/repository/FlightRepositoryImpl.java
+++ src/main/java/projectNomber1/repository/FlightRepositoryImpl.java
@@ -8,9 +8,13 @@ import java.sql.SQLException;
 public class FlightRepositoryImpl implements FlightRepository {
 
     private static final String INSERT_FLIGHT =
-            "insert into flight value(null,'%s',date((now)),1-1-2020)";
+            "insert into flight value(null,'%s',date((now)),'%s','1-1-2020')";
     @Override
     public void extractFlight() throws SQLException {
+
+        Connection connection = ConnectionImpl.getConnection();
+        PreparedStatement statement = connection.prepareStatement(INSERT_FLIGHT);
+        statement.
 
     }
 }
